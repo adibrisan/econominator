5b7815e016dce9a600865d111152c0154eaab8ea
adibrisan <adi.brisan@yahoo.com>
2022-05-08 21:58:20 +0300

    Finished pdf style

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -249,21 +249,261 @@ const HomeScreen = ({ navigation }) => {
   });
   console.log(productsList);
 
-  const pdf = `
-<html>
+  const htmlStyles = `* {
+    border: 0;
+    box-sizing: content-box;
+    color: inherit;
+    font-family: inherit;
+    font-size: inherit;
+    font-style: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    vertical-align: top;
+  }
+  h1 {
+    font: bold 100% sans-serif;
+    letter-spacing: 0.5em;
+    text-align: center;
+    text-transform: uppercase;
+  }
+  /* table */
+  table {
+    font-size: 75%;
+    table-layout: fixed;
+    width: 100%;
+  }
+  table {
+    border-collapse: separate;
+    border-spacing: 2px;
+  }
+  th,
+  td {
+    border-width: 1px;
+    padding: 0.5em;
+    position: relative;
+    text-align: left;
+  }
+  th,
+  td {
+    border-radius: 0.25em;
+    border-style: solid;
+  }
+  th {
+    background: #eee;
+    border-color: #bbb;
+  }
+  td {
+    border-color: #ddd;
+  }
+  /* page */
+  html {
+    font: 16px/1 "Open Sans", sans-serif;
+    overflow: auto;
+  }
+  html {
+    background: #999;
+    cursor: default;
+  }
+  body {
+    box-sizing: border-box;
+    margin: 0 auto;
+    overflow: hidden;
+    padding: 0.25in;
+  }
+  body {
+    background: #fff;
+    border-radius: 1px;
+    box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
+  }
+  /* header */
+  header {
+    margin: 0 0 3em;
+  }
+  header:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+  header h1 {
+    background: #000;
+    border-radius: 0.25em;
+    color: #fff;
+    margin: 0 0 1em;
+    padding: 0.5em 0;
+  }
+  header address {
+    float: left;
+    font-size: 75%;
+    font-style: normal;
+    line-height: 1.25;
+    margin: 0 1em 1em 0;
+  }
+  header address p {
+    margin: 0 0 0.25em;
+  }
+  header span,
+  header img {
+    display: block;
+    float: right;
+  }
+  header span {
+    margin: 0 0 1em 1em;
+    max-height: 25%;
+    max-width: 60%;
+    position: relative;
+  }
+  header img {
+    max-height: 100%;
+    max-width: 100%;
+  }
+  /* article */
+  article,
+  article address,
+  table.meta,
+  table.inventory {
+    margin: 0 0 3em;
+  }
+  article:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+  article h1 {
+    clip: rect(0 0 0 0);
+    position: absolute;
+  }
+  article address {
+    float: left;
+    font-size: 125%;
+    font-weight: bold;
+  }
+  /* table meta & balance */
+  table.meta,
+  table.balance {
+    float: right;
+    width: 36%;
+  }
+  table.meta:after,
+  table.balance:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+  /* table meta */
+  table.meta th {
+    width: 40%;
+  }
+  table.meta td {
+    width: 60%;
+  }
+  /* table items */
+  table.inventory {
+    clear: both;
+    width: 100%;
+  }
+  table.inventory th {
+    font-weight: bold;
+    text-align: center;
+  }
+  table.inventory td:nth-child(1) {
+    width: 26%;
+  }
+  table.inventory td:nth-child(2) {
+    width: 38%;
+  }
+  table.inventory td:nth-child(3) {
+    text-align: right;
+    width: 12%;
+  }
+  /* table balance */
+  table.balance th,
+  table.balance td {
+    width: 50%;
+  }
+  table.balance td {
+    text-align: right;
+  }
+  /* aside */
+  aside h1 {
+    border: none;
+    border-width: 0 0 1px;
+    margin: 0 0 1em;
+  }
+  aside h1 {
+    border-color: #999;
+    border-bottom-style: solid;
+  }
+  `;
+
+  const pdf = `<html>
+
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    <meta charset="utf-8">
+    <title>Your receipt</title>
+    <link rel="license" href="https://www.opensource.org/licenses/mit-license/">
+    <style>
+      ${htmlStyles}
+    </style>
   </head>
-  <body style="text-align: center;">
-    <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
-      ${productNames}
-    </h1>
-    <img
-      src="https://d30j33t1r58ioz.cloudfront.net/static/guides/sdk.png"
-      style="width: 90vw;" />
+  <body>
+    <header>
+      <h1>Your receipt</h1>
+        <p>${user?.displayName ? user?.displayName : user?.name}</p>
+    </header>
+    <article>
+      <address>
+        <p>Econominator</p>
+      </address>
+      <table class="inventory">
+        <thead>
+          <tr>
+            <th><span>Item</span></th>
+            <th><span>Quantity</span></th>
+            <th><span>Price</span></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${(function fun() {
+            let str = "";
+            for (const item of productsList) {
+              str =
+                str +
+                `<tr><td><span>${item.productName}</span></td>
+              <td><span>${item.amount}</span></td>
+              <td><span>${item.price} €</span></td></tr>`;
+            }
+            return str;
+          })()}
+        </tbody>
+      </table>
+      <table class="balance">
+        <tr>
+          <th><span>Total</span></th>
+          ${(function fun() {
+            const totalPrice = productsList.reduce(
+              (acc, item) => Number(acc) + Number(item.price),
+              0
+            );
+            return `<td><span data-prefix>€</span><span> ${totalPrice}</span></td>`;
+          })()}
+        </tr>
+      </table>
+    </article>
+    <aside>
+      <h1><span>Additional Notes</span></h1>
+      <div>
+        <p>Your Econominator team.</p>
+        <br>
+        <p>Today: ${formatDate(date)}</p>
+      </div>
+    </aside>
   </body>
-</html>
-`;
+  
+  </html>`;
 
   const createPDF = async (html) => {
     try {
